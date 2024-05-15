f574cc911add55b5fd329236c69ebdf40b45c52d
Munal Thapa <153220135+Munalthapa710@users.noreply.github.com>
2024-05-15 22:45:28 +0545

    qrforeandk.py
    changing comments

--- qrforeandk.py
+++ qrforeandk.py
@@ -4,15 +4,15 @@ import qrcode
 esewa_id =input("enter your eSewa ID or khalti ID: ")
 name=input("enter your name: ").upper() 
 
-#id or phonenumber
+#or phonenumber
 esewa_url=f'{{"eSewa_id": "{esewa_id}","name":"{name}"}}'
 khalti_url=f'{{"khalti_id": "{esewa_id}","name":"{name}"}}'
 
-#for QR
+#for generating QR
 esewa_qr=qrcode.make (esewa_url)
 khalti_qr=qrcode.make (khalti_url)
 
-#For display
+#For displaying QR
 esewa_khalti=input("Esewa or Khalti ?: ").upper()
 if esewa_khalti=="ESEWA":
    esewa_qr.show()
