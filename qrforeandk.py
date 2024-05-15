e162aee48741db2f9c12397fb8de50a217b57ef7
Munal Thapa <153220135+Munalthapa710@users.noreply.github.com>
2024-05-15 22:43:39 +0545

    Add files via upload

---
+++ qrforeandk.py
@@ -0,0 +1,20 @@
+import qrcode
+
+#taking input 
+esewa_id =input("enter your eSewa ID or khalti ID: ")
+name=input("enter your name: ").upper() 
+
+#id or phonenumber
+esewa_url=f'{{"eSewa_id": "{esewa_id}","name":"{name}"}}'
+khalti_url=f'{{"khalti_id": "{esewa_id}","name":"{name}"}}'
+
+#for QR
+esewa_qr=qrcode.make (esewa_url)
+khalti_qr=qrcode.make (khalti_url)
+
+#For display
+esewa_khalti=input("Esewa or Khalti ?: ").upper()
+if esewa_khalti=="ESEWA":
+   esewa_qr.show()
+else:
+   khalti_qr.show()
